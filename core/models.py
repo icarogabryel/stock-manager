a30bfe5f0c244fdda6b63164ab44f2f750a9c229
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-03-05 22:43:59 -0300

    register Product model in admin and add string representation method

--- core/models.py
+++ core/models.py
@@ -7,3 +7,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
