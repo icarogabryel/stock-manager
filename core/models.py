aeba59332be229b42f9b07a736c783929f3e5860
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-03-05 22:37:15 -0300

    add Product model and initial migration; update .gitignore and settings

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    is_active = models.BooleanField(default=True)
