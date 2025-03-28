93f63b1e9465c5efd378d2e7d644c0a72e0fcd51
Ícaro Gabryel <icarogabryel2001@gmail.com>
2025-03-28 14:37:21 -0300

    change name of the app

--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
+from app import views
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list'),
